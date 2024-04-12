f7f5543690cf741fd5f62ad78fbe9ddd4c69cf69
David Martinez Alcantara <martinezalcantara29@gmail.com>
2024-04-12 13:59:50 +0200

    Trabajando los Shaders

--- Assets/External Packages/PrettyHierarchy-main/Runtime/PrettyObject.cs
+++ Assets/External Packages/PrettyHierarchy-main/Runtime/PrettyObject.cs
@@ -1,48 +1,45 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace PrettyHierarchy
 {
-    [DisallowMultipleComponent]
-    public class PrettyObject : MonoBehaviour
-    {
+	[DisallowMultipleComponent]
+	public class PrettyObject : MonoBehaviour
+	{
 #if UNITY_EDITOR
-        //[Header("Background")]
-        [SerializeField]
-        private bool useDefaultBackgroundColor;
-        [SerializeField]
-        private Color32 backgroundColor = new Color32(255, 255, 255, 255);
-        //[Header("Text")]
-        [SerializeField]
-        private bool useDefaultTextColor;
-        [SerializeField]
-        private Color32 textColor = new Color32(255, 255, 255, 255);
-        [SerializeField]
-        private Font font;
-        [SerializeField]
-        private int fontSize = 12;
-        [SerializeField]
-        private FontStyle fontStyle = FontStyle.Normal;
-        [SerializeField]
-        private TextAnchor alignment = TextAnchor.UpperLeft;
-        [SerializeField]
-        private bool textDropShadow;
+		//[Header("Background")]
+		[SerializeField]
+		private bool useDefaultBackgroundColor;
+		[SerializeField]
+		private Color32 backgroundColor = new(0, 0, 0, 255);
+		//[Header("Text")]
+		[SerializeField]
+		private bool useDefaultTextColor;
+		[SerializeField]
+		private Color32 textColor = new(255, 255, 255, 255);
+		[SerializeField]
+		private Font font;
+		[SerializeField]
+		private int fontSize = 14;
+		[SerializeField]
+		private FontStyle fontStyle = FontStyle.Bold;
+		[SerializeField]
+		private TextAnchor alignment = TextAnchor.MiddleCenter;
+		[SerializeField]
+		private bool textDropShadow;
 
-        public bool UseDefaultBackgroundColor { get { return useDefaultBackgroundColor; } }
-        public Color32 BackgroundColor { get { return new Color32(backgroundColor.r, backgroundColor.g, backgroundColor.b, 255); } }
+		public bool UseDefaultBackgroundColor => useDefaultBackgroundColor;
+		public Color32 BackgroundColor => new(backgroundColor.r, backgroundColor.g, backgroundColor.b, 255);
 
-        public bool UseDefaultTextColor { get { return useDefaultTextColor; } }
-        public Color32 TextColor { get { return textColor; } }
-        public Font Font { get { return font; } }
-        public int FontSize { get { return fontSize; } }
-        public FontStyle FontStyle { get { return fontStyle; } }
-        public TextAnchor Alignment { get { return alignment; } }
-        public bool TextDropShadow { get { return textDropShadow; } }
+		public bool UseDefaultTextColor => useDefaultTextColor;
+		public Color32 TextColor => textColor;
+		public Font Font => font;
+		public int FontSize => fontSize;
+		public FontStyle FontStyle => fontStyle;
+		public TextAnchor Alignment => alignment;
+		public bool TextDropShadow => textDropShadow;
 
-        private void OnValidate()
-        {
-            EditorApplication.RepaintHierarchyWindow();
-        }
+		private void OnValidate() => EditorApplication.RepaintHierarchyWindow();
 #endif
-    }
+	}
 }
